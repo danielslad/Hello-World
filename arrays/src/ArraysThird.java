7b8c6c7f840f8637dde49f046e22ac6a894fec46
Daniel1987Daniel <danielslad@gmail.com>
2021-06-06 17:18:46 +0200

    01.week

---
+++ arrays/src/ArraysThird.java
@@ -0,0 +1,20 @@
+public class ArraysThird {
+    public static void main(String[] args) {
+
+
+        // - Create an array variable named `numbers`
+
+        int[] numbers;
+
+        //   with the following content: `[4, 5, 6, 7]`
+
+        int[] no = {4, 5, 6, 7};
+
+        // - Print the third element of `numbers`
+
+        int[] num = {4, 5, 6, 7};
+
+        System.out.println("The third element of numbers is: " + num[2]);
+
+    }
+}
---
+++ arrays/src/CompareLength.java
@@ -0,0 +1,19 @@
+public class CompareLength {
+    public static void main(String[] args) {
+
+        // - Create an array variable named `firstArrayOfNumbers` with the following content: `[1, 2, 3]`
+
+        int[] firstArrayOfNumbers = {1, 2, 3};
+
+
+
+        // - Create an array variable named `secondArrayOfNumbers with the following content: `[4, 5]`. Print "secondArrayOfNumbers is longer" if `secondArrayOfNumbers` has more elements than `firstArrayOfNumbers`
+        // - Otherwise print: "firstArrayOfNumbers is the longer one"
+
+        int[] secondArrayOfNumbers = {4, 5};
+        System.out.println("firstArrayOfNumbers is the longer one");
+
+
+
+    }
+}
